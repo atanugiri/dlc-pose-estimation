12fc54b734a6458ddc2604b84839dfa84dafd1be
atanu <atanurkm11@gmail.com>
2026-03-11 13:57:38 -0600

    Refactor test script and add batch processing script for video stitching

--- scripts/test.py
+++ scripts/test.py
@@ -1,19 +1,7 @@
-#!/usr/bin/env python3
 from pathlib import Path
 
-cwd = Path(__file__).parent.resolve()
-print(f"Current working directory: {cwd}")
-data_dir = cwd.parent / "Black-ToyLight-Atanu-2026-01-22" / "videos"
-print(f"Data directory: {data_dir}")
-
-if not data_dir.exists():
-    print(f"Data directory does not exist: {data_dir}")
-    exit(1)
-else:
-    print(f"Data directory exists: {data_dir}")
-mp4_videos = sorted([f for f in data_dir.iterdir() if f.is_file() and f.suffix == ".mp4"])
-first_five = [str(f) for f in mp4_videos[:5]]
-# print("First 5 sorted .mp4 videos:")
-for idx, v in enumerate(mp4_videos):
-    file_path = Path(str(v))
-    print(f"{idx}. {file_path.name}")
+if __name__ == "__main__":
+    test_folder = Path("/Users/atanugiri/Downloads/dlc-pose-estimation/Competition-task/Videos")
+    for folder in sorted(test_folder.rglob("*")):
+        if folder.is_dir() and any(folder.glob("*.mp4")):
+            print(folder)
