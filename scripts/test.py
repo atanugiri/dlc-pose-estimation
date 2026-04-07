97cc0ae2ec14ab4011e70740277cb92add0f8373
atanu <atanurkm11@gmail.com>
2026-04-07 10:14:51 -0600

    Update frame selection in config.yaml and remove unused test.py script

--- scripts/test.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-if __name__ == "__main__":
-    test_folder = Path("/Users/atanugiri/Downloads/dlc-pose-estimation/Competition-task/Videos")
-    for folder in sorted(test_folder.rglob("*")):
-        if folder.is_dir() and any(folder.glob("*.mp4")):
-            print(folder)
